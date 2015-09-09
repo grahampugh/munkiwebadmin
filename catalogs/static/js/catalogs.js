758d481ea07a4cf11c3657fefe97fc098782b18a
GR Pugh <glgrp@vpn-user-248-008.nomadic.bris.ac.uk>
2015-09-09 19:44:31 -0400

    Adding catalog edit section to catalog item_detail.html

--- catalogs/static/js/catalogs.js
+++ catalogs/static/js/catalogs.js
@@ -21,3 +21,94 @@ function getCatalogItem(catalog_name, catalog_index, item_name, item_version)   
     $('.catalog_item[name="' + item_name + '"]').addClass('selected');
     $('.catalog_item[name!="' + item_name + '"]').removeClass('selected');
 }
+
+// copied from manifests.js
+
+var inEditMode = false;
+function makeEditableItems(catalog_item_name) {
+    // grab autocomplete data from document
+    var autocomplete_data = $('#data_storage').data('autocomplete_data');
+    // make sections sortable and drag/droppable
+    $('.catalogs_section').sortable();
+    $('.section').sortable({
+        connectWith: '.section'
+    });
+    //replace <a> links with 'editable' divs
+    $('.lineitem').children($('a')).each(function(){
+        var item = "<div class='editable'>" + $(this).parent().attr('id') + "</div>";
+        $(this).replaceWith(item);
+    });
+    $('.lineitem').append("<a href='#' class='btn btn-danger btn-mini lineitem_delete'><i class='icon-minus icon-white'></i><a>");
+    $('.catalog_item_section').on('dblclick', '.editable', function() {
+        makeEditableItem(manifest_name, autocomplete_data, $(this));
+    });
+    $('.catalog_item_section').on('click', '.lineitem_delete', function() {
+      if ($(this).parent().attr('id')) {    
+        var r = confirm("Really delete " + $(this).parent().attr('id') + " from " + $(this).parent().parent().attr('id') + "?");
+        if (r == true){ $(this).parent().remove(); };
+      } else {
+          $(this).parent().remove();
+      }
+    });
+    $('.section_label').append("<a class='btn btn-success btn-mini add_item' href='#'><i class='icon-plus icon-white'></i></a>");
+    $('.add_item').click(function() {
+        var list_item = $("<li class='lineitem'><div class='editable'></div><a href='#' class='btn btn-danger btn-mini lineitem_delete'><i class='icon-minus icon-white'></i><a></li>");
+        $(this).parent().siblings($('ul')).append(list_item);
+        makeEditableItem(
+            manifest_name, autocomplete_data, list_item.children(".editable"));
+    });
+    $('.edit').val('Save').unbind('click').click(function() {
+        getManifestDetailFromDOMAndSave();
+    });
+    $('#save_and_cancel').append("<input type='button' class='cancel btn' value='Cancel' onClick='cancelEdit()'></input>");
+    $(window).bind('beforeunload', function(){
+        return "Changes will be lost!";
+    });
+    inEditMode = true;
+}
+
+function updateLineItem(item) {
+    var text_value = item.val();
+    if (text_value.length) {
+        item.parent().attr('id', text_value);
+        var new_div = $("<div class='editable'>" + text_value + "</div>")
+        item.replaceWith(new_div);
+    } else {
+        item.parent().remove();
+    }
+}
+
+function makeEditableItem(manifest_name, autocomplete_data, editable_div) {
+    // commit any existing active lineiteminput
+    $('.lineiteminput').each(function(){updateLineItem($(this))});
+
+    var text_value = editable_div.text();
+    var input_box = $("<input type='text' id='" + text_value + "' class='lineiteminput' value='" + text_value + "' />");
+    var grandparent_id = editable_div.parent().parent().attr('id');
+    var kind = 'items';
+    if (grandparent_id == 'catalogs') {
+      kind = 'catalogs';
+    } else if (grandparent_id == 'included_manifests') {
+      kind = 'manifests';
+    }
+    editable_div.replaceWith(input_box);
+    input_box.typeahead({source: autocomplete_data[kind]})
+    input_box.focus();
+    input_box.bind('keyup', function(event) {
+        if (event.which == '13' || event.which == '9') {
+            event.preventDefault();
+            updateLineItem($(this));
+        } else if (event.which == '27') {
+            event.preventDefault();
+            $(this).val($(this).attr('id'));
+            updateLineItem($(this));
+        }
+    });
+}
+
+function cancelEdit() {
+    inEditMode = false;
+    $(window).unbind("beforeunload");
+    getManifestDetail();
+}
+
