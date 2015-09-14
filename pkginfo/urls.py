4db126988eca520949c3107b1f08b24d6992cd47
GR Pugh <glgrp@it031540.local>
2015-09-14 17:11:51 -0400

    First commit of pkginfo folder

--- pkginfo/urls.py
+++ pkginfo/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('pkginfo.views',
     url(r'^$', 'index'),
     #url(r'^(?P<pkginfo_name>[^/]+)/(?P<item_name>[^/]+)/$', 'test_index'),
-    url(r'^(?P<pkginfo_name>[^/]+)/edit/$', 'edit'),
+    #url(r'^(?P<pkginfo_name>[^/]+)/edit/$', 'edit'),
 )
