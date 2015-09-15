f190eca8f1c63a57e41fd57b6f44e2705eb8df5c
GR Pugh <glgrp@it031540.local>
2015-09-15 16:19:40 -0400

    Start working out the facility for moving pkgs between catalogs

--- pkginfo/views.py
+++ pkginfo/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django import forms
 
 from models import Pkginfo
 from catalogs.models import Catalog
@@ -8,7 +9,7 @@ from catalogs.models import Catalog
 import os
 
 @login_required
-def index(request):
+def index(self):
     all_catalog_items = Pkginfo.detail()
     catalog_list = Catalog.list()
     catalog_name = 'none'
@@ -26,23 +27,14 @@ def index(request):
                                }
                               )
 
-
-
 # @login_required
-# def item_detail(request, item_index):
-#     catalog_item = Pkginfo.item_detail(item_index)
-#     featured_keys = ['name', 'version', 'catalogs']
-#     # sort the item by key so keys are displayed
-#     # in expected order
-#     sorted_dict = SortedDict()
-#     for key in featured_keys:
-#         if key in catalog_item:
-#             sorted_dict[key] = catalog_item[key]
-#     key_list = catalog_item.keys()
-#     key_list.sort()
-#     for key in key_list:
-#         if key not in featured_keys:
-#             sorted_dict[key] = catalog_item[key]
-#     return render_to_response('pkginfo/index.html', 
-#                               {'catalog_item': sorted_dict})
-#                               
+# def confirm(request):
+#     if request.method == 'POST': # If the form has been submitted...
+#         catalog = request.POST.get('catalog')
+#         checked = request.POST.getlist('items_to_move[]')
+        # for each item in checked
+            # split back to version and string
+            # look up path to file in catalog details
+            # edit catalogs field in the file
+            # set a variable to state that something has changed
+        # if something has changed, makecatalogs
