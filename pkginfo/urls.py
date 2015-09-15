1c6d4cd8610daeae5aab3e996b762c4bd45696c2
GR Pugh <glgrp@it031540.local>
2015-09-15 10:47:20 -0400

    Adding catalog edit section to catalog item_detail.html

--- pkginfo/urls.py
+++ pkginfo/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('pkginfo.views',
-    url(r'^index/*$', 'index'),
-    url(r'^$', 'index'),
-    #url(r'^(?P<pkginfo_name>[^/]+)/(?P<item_name>[^/]+)/$', 'test_index'),
-    #url(r'^(?P<pkginfo_name>[^/]+)/edit/$', 'edit'),
+	url(r'^index/*$', 'index'),
+	url(r'^$', 'index'),
+	#url(r'^(?P<pkginfo_name>[^/]+)/(?P<item_name>[^/]+)/$', 'test_index'),
+	#url(r'^(?P<pkginfo_name>[^/]+)/edit/$', 'edit'),
 )
